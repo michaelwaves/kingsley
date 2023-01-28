71a5656ab6865930ffbdadd66c3ed06673480d82
michaelwaves <68243408+michaelwaves@users.noreply.github.com>
2023-01-28 14:39:08 -0500

    chatbox component

--- react/src/views/Chatbot.jsx
+++ react/src/views/Chatbot.jsx
@@ -6,24 +6,59 @@ import { useState } from "react";
 import mic from "./images/mic-black.svg"
 import send from "./images/send-1-svgrepo-com.svg"
 import speaker from "./images/speaker-wave-2.svg"
+import React from "react";
+import ChatBox from "./components/ChatBox";
 
+class AppComponent extends React.Component {
+    state = {
+        numChildren: 0
+    }
 
+    render() {
+        const children = [];
+
+        for (var i = 0; i < this.state.numChildren; i += 1) {
+            children.push(<ChildComponent key={i} number={i} />);
+        };
+
+        return (
+            <ParentComponent addChild={this.onAddChild} children={children} />
+        );
+    }
+
+    onAddChild = () => {
+        this.setState({
+            numChildren: this.state.numChildren + 1
+        });
+    }
+}
+
+const ParentComponent = props => (
+    <div className="card calculator">
+        <p><a href="#" onClick={props.addChild}>Add Another Child Component</a></p>
+        <div id="children-pane">
+            {props.children}
+        </div>
+    </div>
+);
+
+const ChildComponent = props => <div>{"I am child " + props.number}</div>;
 
 function Chatbot() {
     const [title, setTitle] = useState("")
     const commentData = [{ "user": "human", "text": "I love Kingston" },
     { "user": "ai", "text": "me too" },
     { "user": "human", "text": "let's move to Kingston" }]
-    const [data, setData] = useState(commentData)
 
     return (
         <div className="chatbot">
             <Bar />
             <div className="chatbot-body" >
-                {data.map((comment) => comment.user === "human" ? <Comment text={comment.text} /> : <AIComment text={comment.text} />)}
+                {commentData.map((comment) => comment.user === "human" ? <Comment text={comment.text} /> : <AIComment text={comment.text} />)}
                 <Comment text="what is kingston?" />
                 <AIComment text="kingston is a small city beside lake ontario" />
                 <Comment text={title} />
+                <ChatBox text={title} />
             </div>
             <div className="input-bar">
                 <input type="image" src={mic} alt="whoops" className="icon" />
---
+++ react/src/views/components/ChatBox.jsx
@@ -0,0 +1,28 @@
+import Comment from "./Comment";
+import AIComment from "./AIComment";
+import { useState } from "react";
+import send from "../images/send-1-svgrepo-com.svg"
+
+function ChatBox(props) {
+    const [numChildren, setNumChildren] = useState(0)
+    const children = []
+
+    for (var i = 0; i < numChildren; i++) {
+        children.push(<Comment text={props.text} />)
+    }
+    return (
+        <div>
+            {children}
+            <input
+                type="image"
+                src={send}
+                alt="whoops"
+                className="icon"
+                onClick={() => setNumChildren(numChildren + 1)}
+
+            />
+        </div>
+    )
+}
+
+export default ChatBox
