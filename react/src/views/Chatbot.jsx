3b3f377a9afcce9913a05852705902d209c54820
Stuart Fong <stuartfong1@gmail.com>
2023-01-28 15:19:21 -0500

    Add voice input

--- react/src/views/Chatbot.jsx
+++ react/src/views/Chatbot.jsx
@@ -7,7 +7,19 @@ import mic from "./images/mic-black.svg"
 import send from "./images/send-1-svgrepo-com.svg"
 import speaker from "./images/speaker-wave-2.svg"
 
+import MicRecorder from "mic-recorder-to-mp3"
+import { useEffect, useRef } from "react"
+import axios from "axios"
 
+// Set AssemblyAI Axios Header
+const assembly = axios.create({
+    baseURL: "https://api.assemblyai.com/v2",
+    headers: {
+      authorization: "INSERT API KEY",
+      "content-type": "application/json",
+      "transfer-encoding": "chunked",
+    },
+  })
 
 function Chatbot() {
     const [title, setTitle] = useState("")
@@ -15,6 +27,101 @@ function Chatbot() {
     { "user": "ai", "text": "me too" },
     { "user": "human", "text": "let's move to Kingston" }]
     const [data, setData] = useState(commentData)
+
+    // Mic-Recorder-To-MP3
+    const recorder = useRef(null) //Recorder
+    const audioPlayer = useRef(null) //Ref for the HTML Audio Tag
+    const [blobURL, setBlobUrl] = useState(null)
+    const [audioFile, setAudioFile] = useState(null)
+    const [isRecording, setIsRecording] = useState(null)
+
+    useEffect(() => {
+        //Declares the recorder object and stores it inside of ref
+        recorder.current = new MicRecorder({ bitRate: 128 })
+    }, [])
+
+    const startRecording = () => {
+    // Check if recording isn't blocked by browser
+    recorder.current.start().then(() => {
+        setIsRecording(true)
+    })
+    }
+
+    const stopRecording = () => {
+        recorder.current
+          .stop()
+          .getMp3()
+          .then(([buffer, blob]) => {
+            const file = new File(buffer, "audio.mp3", {
+              type: blob.type,
+              lastModified: Date.now(),
+            })
+            const newBlobUrl = URL.createObjectURL(blob)
+            setBlobUrl(newBlobUrl)
+            setIsRecording(false)
+            setAudioFile(file)
+          })
+          .catch((e) => console.log(e))
+        
+        submitTranscriptionHandler()
+      }
+    
+      const [uploadURL, setUploadURL] = useState("")
+      const [transcriptID, setTranscriptID] = useState("")
+      const [transcriptData, setTranscriptData] = useState("")
+      const [transcript, setTranscript] = useState("")
+      const [isLoading, setIsLoading] = useState(false)
+    
+      // Upload the Audio File and retrieve the Upload URL
+      useEffect(() => {
+        if (audioFile) {
+          assembly
+            .post("/upload", audioFile)
+            .then((res) => setUploadURL(res.data.upload_url))
+            .catch((err) => console.error(err))
+        }
+      }, [audioFile])
+    
+      // Submit the Upload URL to AssemblyAI and retrieve the Transcript ID
+      const submitTranscriptionHandler = () => {
+        assembly
+          .post("/transcript", {
+            audio_url: uploadURL,
+          })
+          .then((res) => {
+            setTranscriptID(res.data.id)
+    
+            checkStatusHandler()
+          })
+          .catch((err) => console.error(err))
+      }
+    
+      // Check the status of the Transcript and retrieve the Transcript Data
+      const checkStatusHandler = async () => {
+        setIsLoading(true)
+        try {
+          await assembly.get(`/transcript/${transcriptID}`).then((res) => {
+            setTranscriptData(res.data)
+            setTranscript(transcriptData.text)
+          })
+        } catch (err) {
+          console.error(err)
+        }
+      }
+
+      useEffect(() => {
+        const interval = setInterval(() => {
+          if (transcriptData.status !== "completed" && isLoading) {
+            checkStatusHandler()
+          } else {
+            setIsLoading(false)
+            setTranscript(transcriptData.text)
+            setTitle(transcript)
+            clearInterval(interval)
+          }
+        }, 1000)
+        return () => clearInterval(interval)
+      },)
 
     return (
         <div className="chatbot">
@@ -26,7 +133,7 @@ function Chatbot() {
                 <Comment text={title} />
             </div>
             <div className="input-bar">
-                <input type="image" src={mic} alt="whoops" className="icon" />
+                <input type="image" src={mic} alt="whoops" className="icon" onClick={isRecording ? stopRecording : startRecording} />
                 <input type="image" src={speaker} alt="whoops" className="icon" />
                 <input
                     type="text"
