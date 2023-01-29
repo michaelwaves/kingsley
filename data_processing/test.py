404877b33b27edfef11474fc3b54a22be25fc844
BCheng-KH <brandonkhcheng123@gmail.com>
2023-01-29 06:58:22 -0500

    all is well now

--- data_processing/test.py
+++ data_processing/test.py
@@ -1,4 +1,6 @@
 # import json
+import openai
+openai.api_key = "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"
 
 # training_file = ".\\data_processing\\train_data_with_context.jsonl"
 # training_prompts = []
@@ -8,9 +10,20 @@
 #         training_prompts.append(json.loads(line))
 # print(training_prompts[25]["prompt"])
 import get_sim
-
+model = "ada:ft-university-of-toronto-2023-01-29-06-16-22"
+def clean_response(response):
+    text = response["choices"][0]['text']
+    text1 = text[:text.find('###')]
+    text2 = text[:text.find('###')+3]
+    return text1, text2
 while True:
     question = input("what is your question?\n")
     prompt = get_sim.create_prompt(question)
     
-    print(prompt)
+    response =  openai.Completion.create(
+            model=model,
+            prompt=prompt,
+            max_tokens=256,
+            temperature=0
+            )
+    print(clean_response(response))
